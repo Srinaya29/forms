a4b393500aba11718c5aa97d8de1d0897acbf727
Srinaya29 <sydugarisrinaya@gmail.com>
2025-03-05 13:52:12 +0530

    updated

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-import greet from "./calc.js"
-import {add} from "./calc.js"
-let result = greet()
-console.log(result)
+import greet, { add } from "./calc.js"; 
+
+let result = greet();
+console.log(result);  // Output: Hello
+
+let sum = add(5, 3);
+console.log(sum);  // Output: 8
